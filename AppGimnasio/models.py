eebd6169954e671cf759fdcabf0ff9496a16ec23
Daniela Erario <danielaerarioo@gmail.com>
2023-10-05 19:04:41 -0300

    Test

--- AppGimnasio/models.py
+++ AppGimnasio/models.py
@@ -6,14 +6,22 @@ class Alumno(models.Model):
     apellido = models.CharField(max_length=40)
     edad = models.IntegerField()
     email = models.EmailField()
+    
+    def __str__(self): #Esta funcion con __str__ es para que el panel se vea mas lindo.
+        return f"Nombre: {self.nombre} - Apellido {self.apellido} - Edad {self.edad} - E-Mail {self.email}"
+
 
 class Clase(models.Model):
     clase = models.CharField(max_length=40)
     dia = models.DateField()
     tiempo = models.IntegerField()
-    
+    def __str__(self):
+        return f"Clase: {self.clase} - Dia {self.dia} - Tiempo {self.tiempo}"
+
 class Sucursal(models.Model):
     ciudad = models.CharField(max_length=40)
     calle = models.CharField(max_length=20)
     numero = models.IntegerField()
+    def __str__(self):
+        return f"Ciudad: {self.ciudad} - Calle {self.calle} - Numero {self.numero}"
 
